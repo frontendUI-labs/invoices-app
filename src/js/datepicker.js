bc7cb50cecc98ad34cd8fb8268ed24059c675ecb
Emerson <emerzon.lc@gmail.com>
2023-05-17 18:29:55 -0500

    feat:display current day in calendar

--- src/js/datepicker.js
+++ src/js/datepicker.js
@@ -1,63 +1,123 @@
 import getDaysInMonth from 'date-fns/getDaysInMonth';
 import getDay from 'date-fns/getDay';
-import addMonths from 'date-fns/addMonths';
 import lastDayOfMonth from 'date-fns/lastDayOfMonth';
 import sub from 'date-fns/sub';
 import format from 'date-fns/format';
-import startOfDay from 'date-fns/startOfDay';
 import weeksToDays from 'date-fns/weeksToDays';
-import getMonth from 'date-fns/getMonth';
+import addMonths from 'date-fns/addMonths';
+import startOfMonth from 'date-fns/startOfMonth';
 
-//bota el dia que pones//
-const currentDate = new Date(2023, 11);
+// renderCalendarDays(month, year);
+const datepickerEl = document.querySelector('#datepicker');
+const daysEl = datepickerEl.querySelector('.datepicker__days');
+let currentDateUserSelected = new Date(); //bota el dia que escoje el usuario//
+let currentDateCalendar = new Date(); //bota el dia actual//
+//DIAS//
+function renderCalendarDays() {
+  const startDayInAMonth = startOfMonth(currentDateCalendar); // bota el primer del mes//
+  const starDayOfMonth = getDay(startDayInAMonth); // bota la posicion que cae el primer dia//
+  const daysInAMonth = getDaysInMonth(currentDateCalendar); //bota la cantidad de dias que tiene el mes//
+  const previewMonthDate = sub(currentDateCalendar, {
+    //bota el mes anterior (1)//
+    months: 1,
+  });
+  const lastPrevDate = lastDayOfMonth(previewMonthDate); //bota el ultimo dia del mes anterior//
+  const lastDayOfPrevMonth = format(lastPrevDate, 'd'); //bota la cantidad de dias del mes anterior//
 
-// bota la posicion que cae el dia//
-const starDayOfMonth = getDay(currentDate);
-//bota la cantidad de dias que tiene el mes//
-const daysInAMonth = getDaysInMonth(currentDate);
-// bota el primer dia del mes anterior//
-const previewMonthDate = sub(currentDate, {
-  months: 1,
+  // sale 25 : cantidad de dias del mes anterior(31)- posicion del primer dia del mes actual(6)
+  const initialPrevDate = lastDayOfPrevMonth - starDayOfMonth;
+  const lastCurrentDate = lastDayOfMonth(currentDateCalendar); //bota el ultimo dia del mes//
+  const lastDayCurrentDate = format(lastCurrentDate, 'd'); //formato de los dias agarrados//
+  const getCurrentLastDay = getDay(lastCurrentDate); //bota la posicion del ultimo dia del mes//
+  console.log('getCurrentLastDay', getCurrentLastDay);
+
+  //dias previos al mes actual//
+  const prevDays = [...Array(starDayOfMonth).keys()].map(
+    (i) => i + initialPrevDate + 1
+  ); //crea el array de espacios a rellenar//
+
+  // dias actuales del mes//
+  const dayArray = [...Array(daysInAMonth).keys()]; //crea el array de espacios a rellenar//
+  const currentDays = dayArray.map((i) => {
+    return i + 1; // contando desde el "1" (i +1)//
+  });
+
+  //dias posteriores al mes actual//
+  const daysInAWeek = weeksToDays(1); //cantidad de dias de una semana//
+  const lastNextDate = daysInAWeek - getCurrentLastDay - 1; // cantidad de espacios que ocupa los dias post.//
+  const nextDays = [...Array(lastNextDate).keys()].map((i) => i + 1); // crea el array de espacios a rellenar//
+
+  //(...) junta a los dias previos + actuales + posteriores//
+  const allDays = [...prevDays, ...currentDays, ...nextDays];
+
+  allDays.forEach((item, idx) => {
+    const isInactiveDay =
+      idx < starDayOfMonth ||
+      idx >= prevDays.length + Number(lastDayCurrentDate);
+
+    const spanEl = document.createElement('span');
+
+    spanEl.setAttribute('role', 'button');
+    spanEl.setAttribute('tabindex', 0);
+    spanEl.classList.add('text-h4', 'datepicker-day');
+
+    if (isInactiveDay) {
+      spanEl.classList.add('is-inactive');
+    }
+
+    if (
+      format(currentDateUserSelected, 'd') == item &&
+      format(currentDateUserSelected, 'M') ==
+        format(currentDateCalendar, 'M') &&
+      format(currentDateUserSelected, 'yyyy') ==
+        format(currentDateCalendar, 'yyyy') &&
+      !isInactiveDay
+    ) {
+      spanEl.classList.add('current');
+    }
+
+    spanEl.textContent = item;
+    daysEl.append(spanEl);
+  });
+}
+
+//TITLE CALENDAR//
+const calendarTitleEl = datepickerEl.querySelector('.calendar__title');
+function renderCalendarTitle() {
+  const formattedCurrentDate = format(currentDateCalendar, '	MMM yyyy'); //formato de mes y a~o//
+  calendarTitleEl.textContent = formattedCurrentDate;
+}
+function renderCalendaValue() {
+  const datepickerTitleEl = datepickerEl.querySelector('.datepicker__value');
+
+  const allFormattedCurrentDate = format(currentDateCalendar, 'd MMM yyyy'); //formato de dia mes y a~o//
+  datepickerTitleEl.textContent = allFormattedCurrentDate;
+  console.log(allFormattedCurrentDate);
+}
+
+//FUNCTIONS//
+renderCalendarTitle();
+
+renderCalendarDays();
+renderCalendaValue();
+
+//ARROWS//
+const prevMonthEl = datepickerEl.querySelector('#prevMonth');
+const nextMonthEl = datepickerEl.querySelector('#nextMonth');
+nextMonthEl.addEventListener('click', () => {
+  currentDateCalendar = addMonths(currentDateCalendar, 1); // agrega una cantidad de meses posteriores al actual//
+  renderCalendarTitle();
+  daysEl.innerHTML = '';
+
+  renderCalendarDays();
 });
-//bota el ultimo dia del mes anterior//
-const lastPrevDate = lastDayOfMonth(previewMonthDate);
-//bota la cantidad de dias del mes anterior//
-const lastDayOfPrevMonth = format(lastPrevDate, 'd');
+prevMonthEl.addEventListener('click', () => {
+  currentDateCalendar = sub(currentDateCalendar, {
+    //bota el mes anterior (1)//
+    months: 1,
+  });
+  renderCalendarTitle();
+  daysEl.innerHTML = '';
 
-// sale 25 : cantidad de dias del mes anterior(31)- posicion del primer dia del mes actual(6)
-const initialPrevDate = lastDayOfPrevMonth - starDayOfMonth;
-
-//bota el ultimo dia del mes//
-const lastCurrentDate = lastDayOfMonth(currentDate);
-const lastDayCurrentDate = format(lastCurrentDate, 'd');
-
-//bota la posicion del ultimo dia del mes//
-const getCurrentLastDay = getDay(lastCurrentDate);
-
-const prevDays = [...Array(starDayOfMonth).keys()].map(
-  (i) => i + initialPrevDate + 1
-);
-
-const dayArray = [...Array(daysInAMonth).keys()];
-const currentDays = dayArray.map((i) => {
-  return i + 1;
+  renderCalendarDays();
 });
-
-const daysInAWeek = weeksToDays(1);
-const lastNextDate = daysInAWeek - getCurrentLastDay - 1;
-const nextDays = [...Array(lastNextDate).keys()].map((i) => i + 1);
-
-const allDays = [...prevDays, ...currentDays, ...nextDays];
-const allDaysElements = allDays.map((item, idx) => {
-  return `<span role="button" tabindex="0" class="text-h4 datepicker-day ${
-    idx < starDayOfMonth || idx >= prevDays.length + Number(lastDayCurrentDate)
-      ? 'is-inactive'
-      : ''
-  } ">${item}</span>`;
-});
-
-const allDaysElementsString = allDaysElements.join('');
-
-const monthsDaysEl = document.querySelector('#datepicker');
-const dayEl = monthsDaysEl.querySelector('.datepicker__days');
-dayEl.innerHTML = allDaysElementsString;
